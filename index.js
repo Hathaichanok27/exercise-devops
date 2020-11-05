e0f4e3229e5adfc613c33a06c3536dc732642b04
Hathaichanok27 <60160008@go.buu.ac.th>
2020-11-05 19:05:20 +0700

    add test GET '/'

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+    return res.send('Hello!')
+})
+
 app.listen(port, () => {
     console.log(`Start Express application at ${port}`)
 })
